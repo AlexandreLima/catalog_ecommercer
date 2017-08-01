b609a73ac82dc53cbb221a34ac2c9a83892bffe8
alexandre <xandebre@gma>
2017-07-31 23:00:59 -0300

    Inclusão de lógica para consulta de Sku

--- Infraestruture/Data/Repository/Impl/Mongo/Sku/SkuRepository.cs
+++ Infraestruture/Data/Repository/Impl/Mongo/Sku/SkuRepository.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using EcommercerCatalog.Infraestruture.Data.Repository.Contracts.Sku;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 
 namespace EcommercerCatalog.Infraestruture.Data.Repository.Impl.Mongo.Sku
 {
@@ -10,6 +15,8 @@ namespace EcommercerCatalog.Infraestruture.Data.Repository.Impl.Mongo.Sku
         private MongoClient _mongoClient;
         private MongoServer _mongoServer;
         private MongoDatabase _mongoDatabase;
+        public IMongoCollection<Model.Catalog.Sku> Collection;
+        private const string collectionName = "SkuCollection"; 
         
         public SkuRepository()
         {
@@ -22,7 +29,7 @@ namespace EcommercerCatalog.Infraestruture.Data.Repository.Impl.Mongo.Sku
         {
             try
             {
-                _mongoDatabase.GetCollection("SkuCollection").Save(sku);
+                _mongoDatabase.GetCollection(collectionName).Save(sku);
             }
             catch (Exception e)
             {
@@ -30,6 +37,13 @@ namespace EcommercerCatalog.Infraestruture.Data.Repository.Impl.Mongo.Sku
                 throw;
             }
             
+        }
+
+        public List<Model.Catalog.Sku> GetAllBySearchQuery(string searchQuery)
+        {
+            var collection = _mongoDatabase.GetCollection<Model.Catalog.Sku>(collectionName);
+            var filter = Builders<Model.Catalog.Sku>.Filter.Eq("FindCategory", searchQuery);
+            return collection.Find(Query.EQ("FindCategory", searchQuery)).ToList();
         }
     }
 }
